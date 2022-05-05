e0ea2f7a4de902759cd7e21b0f2f62a4949429e9
Kistaf <Jeti544@gmail.com>
2022-05-05 11:24:42 +0200

    Finished

--- src/com/company/Controller.java
+++ src/com/company/Controller.java
@@ -1,8 +1,32 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Controller {
 
+    public String readLine_str(String str) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print(str);
+        return scanner.nextLine();
+    }
 
+    public double readline_double(String str) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print(str);
+        return scanner.nextDouble();
+    }
+
+    public double calculateEBITA(double amount, double price) {
+        return price*amount;
+    }
+
+    public double calculateEBITAWithDiscount(double EBITA, double discount) {
+        return EBITA * (1 - (discount) / 100);
+    }
+
+    public double calculateTotalWithTax(double EBITA, double tax) {
+        return EBITA * (1 + (tax) / 100);
+    }
 
 
     public double statecodeToTaxBracket(String code) {
@@ -12,19 +36,37 @@ public class Controller {
             case "TX" -> {return 6.25;}
             case "AL" -> {return 4.00;}
             case "CA" -> {return 8.25;}
-            default -> {return 0.0;}
+            default ->   {return 0;}
         }
     }
 
     public int discountFromValue(double value) {
-        if (value >= 50000) return 15;
-        else if (value >= 10000) return 10;
-        else if (value >= 7000) return 7;
-        else if (value >= 5000) return 5;
-        else if (value >= 3000) return 3;
-        else return 0;
+        if          (value >= 50000)    return 15;
+        else if     (value >= 10000)    return 10;
+        else if     (value >= 7000)     return 7;
+        else if     (value >= 5000)     return 5;
+        else if     (value >= 1000)     return 3;
+        else                            return 0;
     }
 
+
+
+
     public void run() {
+        double packageAmount    =          readline_double("Amount of items: ");
+        double price            =          readline_double("Price per item: ");
+        String stateCode        =          readLine_str("State code: ");
+        double EBITA            =          calculateEBITA(packageAmount, price);
+        double possibleDiscount =          discountFromValue(EBITA);
+        double possibleTax      =          statecodeToTaxBracket(stateCode);
+
+        EBITA                   =          calculateEBITAWithDiscount(EBITA, possibleDiscount);
+        double total            =          calculateTotalWithTax(EBITA, possibleTax);
+
+        System.out.println("Total price: " + total);
+
+
+
+
     }
 }
